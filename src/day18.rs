90320f9b3fedea3a1583a7daf58cf08bbeec894a
Carlos Macasaet <github@carlos-m.net>
2021-07-02 15:46:50 -0700

    Day 18 - Part 2

--- src/day18.rs
+++ src/day18.rs
@@ -11,6 +11,7 @@ pub enum Operator {
     Multiply,
 }
 
+#[derive(Eq, PartialEq)]
 pub enum Token {
     Number(Int),
     StartGroup,
@@ -19,10 +20,10 @@ pub enum Token {
     Times,
 }
 
-pub fn get_input() -> impl Iterator<Item=Vec<Token>> {
-    get_lines("day-18-input.txt")
-        .map(|line| -> Vec<Token> {
-            line.chars().flat_map(|c| -> Option<Token> {
+pub fn get_input() -> impl Iterator<Item = Vec<Token>> {
+    get_lines("day-18-input.txt").map(|line| -> Vec<Token> {
+        line.chars()
+            .flat_map(|c| -> Option<Token> {
                 match c {
                     '0'..='9' => Some(Token::Number(c.to_digit(10).unwrap() as Int)),
                     '+' => Some(Token::Plus),
@@ -32,8 +33,9 @@ pub fn get_input() -> impl Iterator<Item=Vec<Token>> {
                     ' ' => None,
                     _ => panic!("Invalid char: {}", c),
                 }
-            }).collect()
-        })
+            })
+            .collect()
+    })
 }
 
 pub struct Frame {
@@ -43,40 +45,44 @@ pub struct Frame {
 
 #[cfg(test)]
 mod tests {
-    use crate::day18::{get_input, Int, Token, Frame, Operator};
     use crate::day18::Operator::{Add, Multiply};
+    use crate::day18::{get_input, Frame, Int, Operator, Token};
 
     #[test]
     fn part1() {
-        let sum = get_input().map(|tokens| -> Int {
-            let mut stack: Vec<Frame> = vec![];
+        let sum = get_input()
+            .map(|tokens| -> Int {
+                let mut stack: Vec<Frame> = vec![];
 
-            let mut total: Option<Int> = None;
-            let mut operator: Option<Operator> = None;
+                let mut total: Option<Int> = None;
+                let mut operator: Option<Operator> = None;
 
-            tokens.iter().for_each(|token| {
-                match token {
-                    Token::Number(i) => {
-                        match (total, operator.as_ref()) {
-                            (None, None) => {
-                                total = Some(*i);
-                            },
-                            (Some(left_value), Some(op)) => {
-                                total = Some(match op {
-                                    Operator::Add => left_value + i,
-                                    Operator::Multiply => left_value * i,
-                                });
-                                operator = None;
-                            },
-                            (left_value, operator) => {
-                                panic!("Invalid state: {:?} {:?}", left_value, operator);
-                            }
+                tokens.iter().for_each(|token| match token {
+                    Token::Number(i) => match (total, operator.as_ref()) {
+                        (None, None) => {
+                            total = Some(*i);
                         }
-                    }
+                        (Some(left_value), Some(op)) => {
+                            total = Some(match op {
+                                Operator::Add => left_value + i,
+                                Operator::Multiply => left_value * i,
+                            });
+                            operator = None;
+                        }
+                        (left_value, operator) => {
+                            panic!("Invalid state: {:?} {:?}", left_value, operator);
+                        }
+                    },
                     Token::StartGroup => {
                         let frame = match (total, operator) {
-                            (None, None) => Frame { left_value: 0, operator: Add },
-                            (Some(left_value), Some(operator)) => Frame{ left_value, operator },
+                            (None, None) => Frame {
+                                left_value: 0,
+                                operator: Add,
+                            },
+                            (Some(left_value), Some(operator)) => Frame {
+                                left_value,
+                                operator,
+                            },
                             (_, _) => panic!("Cannot start group given {:?} {:?}", total, operator),
                         };
                         stack.push(frame);
@@ -84,8 +90,17 @@ mod tests {
                         operator = None;
                     }
                     Token::EndGroup => {
-                        assert!(total.is_some(), "Group has no value: {:?} {:?}", total, operator);
-                        assert!(operator.is_none(), "Group cannot be closed while {:?} operator is unresolved.", operator);
+                        assert!(
+                            total.is_some(),
+                            "Group has no value: {:?} {:?}",
+                            total,
+                            operator
+                        );
+                        assert!(
+                            operator.is_none(),
+                            "Group cannot be closed while {:?} operator is unresolved.",
+                            operator
+                        );
 
                         let partial = total.unwrap();
                         let frame = stack.pop().expect("No frame on the stack");
@@ -96,24 +111,103 @@ mod tests {
                     }
                     Token::Plus => {
                         assert!(total.is_some(), "No left_value for '+'");
-                        assert!(operator.is_none(), "'+' cannot follow {:?}", operator.as_ref().unwrap());
+                        assert!(
+                            operator.is_none(),
+                            "'+' cannot follow {:?}",
+                            operator.as_ref().unwrap()
+                        );
                         operator = Some(Add);
                     }
                     Token::Times => {
                         assert!(total.is_some(), "No left_value for '*'");
-                        assert!(operator.is_none(), "'*' cannot follow {:?}", operator.as_ref().unwrap());
+                        assert!(
+                            operator.is_none(),
+                            "'*' cannot follow {:?}",
+                            operator.as_ref().unwrap()
+                        );
                         operator = Some(Multiply);
                     }
-                }
-            });
-            total.expect("Could not evaluate expression")
-        }).sum::<Int>();
+                });
+                total.expect("Could not evaluate expression")
+            })
+            .sum::<Int>();
 
         println!("Part 1: {}", sum);
     }
 
-    // #[test]
+    #[test]
     fn part2() {
-        assert!(false)
+        let sum = get_input()
+            .map(move |tokens| -> Vec<Token> {
+                let mut operators: Vec<Token> = vec![];
+                let mut result = vec![];
+                for token in tokens {
+                    match token {
+                        Token::Number(_) => result.push(token),
+                        Token::StartGroup => operators.push(token),
+                        Token::EndGroup => loop {
+                            let operator = operators.pop().expect("");
+                            match operator {
+                                Token::StartGroup => break,
+                                _ => {
+                                    assert!(!operators.is_empty(), "Missing opening parenthesis");
+                                    result.push(operator)
+                                }
+                            }
+                        },
+                        current_operator => {
+                            while let Some(previous_operator) = operators.last() {
+                                match previous_operator {
+                                    Token::Number(_) => {
+                                        panic!("Found a number in the operator stack.")
+                                    }
+                                    Token::StartGroup => break,
+                                    Token::EndGroup => {
+                                        panic!("Found an group end in the operator stack.")
+                                    }
+                                    previous_operator => {
+                                        if *previous_operator == Token::Plus
+                                            || (*previous_operator == Token::Times
+                                                && current_operator == Token::Times)
+                                        {
+                                            result.push(operators.pop().unwrap());
+                                        } else {
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+                            operators.push(current_operator);
+                        }
+                    }
+                }
+                while !operators.is_empty() {
+                    result.push(operators.pop().unwrap());
+                }
+                result
+            })
+            .map(move |tokens| -> Int {
+                let mut stack = vec![];
+                for token in tokens {
+                    match token {
+                        Token::Number(i) => stack.push(i),
+                        Token::StartGroup => panic!("group start unexpected"),
+                        Token::EndGroup => panic!("group end unexpected"),
+                        Token::Plus => {
+                            let right_value = stack.pop().unwrap();
+                            let left_value = stack.pop().unwrap();
+                            stack.push(left_value + right_value);
+                        }
+                        Token::Times => {
+                            let right_value = stack.pop().unwrap();
+                            let left_value = stack.pop().unwrap();
+                            stack.push(left_value * right_value);
+                        }
+                    }
+                }
+                stack.pop().unwrap()
+            })
+            .sum::<Int>();
+        println!("Part 2: {}", sum);
     }
 }
