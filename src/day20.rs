64224d02b2ed13d96f649961af574f5de0596e0e
Carlos Macasaet <github@carlos-m.net>
2021-09-18 23:27:20 -0700

    Day 20 - Part 2
    Naïve implementation

--- src/day20.rs
+++ src/day20.rs
@@ -5,7 +5,7 @@ use std::fmt::{Display, Formatter};
 
 use crate::get_lines;
 
-type Id=u64;
+type Id = u64;
 
 pub struct Tile {
     id: Id,
@@ -34,7 +34,7 @@ impl Tile {
              /* these are redundant:
              original.rotate180().flip_horizontally(), original.rotate180().flip_vertically(),
              original.rotate270().flip_horizontally(), original.rotate270().flip_vertically(),
-              */
+             */
              original,
         ]
     }
@@ -70,12 +70,20 @@ impl Clone for OrientedTile {
 }
 
 impl OrientedTile {
+
+    const SEA_MONSTER: [[char; 20]; 3] = [
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ],
+        ['#', ' ', ' ', ' ', ' ', '#', '#', ' ', ' ', ' ', ' ', '#', '#', ' ', ' ', ' ', ' ', '#', '#', '#', ],
+        [' ', '#', ' ', ' ', '#', ' ', ' ', '#', ' ', ' ', '#', ' ', ' ', '#', ' ', ' ', '#', ' ', ' ', ' ', ],
+    ];
+
     fn left_border(&self) -> Vec<char> { // TODO can we return Iterator instead of Vec?
         self.pixels
             .iter()
             .map(|row| row[0])
             .collect()
     }
+
     fn right_border(&self) -> Vec<char> {
         let last_index = self.pixels.len() - 1;
         self.pixels
@@ -83,13 +91,16 @@ impl OrientedTile {
             .map(|row| row[last_index])
             .collect()
     }
+
     fn top_border(&self) -> Vec<char> {
         self.pixels[0].clone()
     }
+
     fn bottom_border(&self) -> Vec<char> {
         let last_index = self.pixels.len() - 1;
         self.pixels[last_index].clone()
     }
+
     fn flip_horizontally(&self) -> Self {
         let pixels = self.pixels
             .iter()
@@ -152,6 +163,7 @@ impl OrientedTile {
     fn fits_to_left_of(&self, right_candidate: &OrientedTile) -> bool {
         OrientedTile::edges_match(&right_candidate.left_border(), &self.right_border())
     }
+
     fn fits_above(&self, bottom_candidate: &OrientedTile) -> bool {
         OrientedTile::edges_match(&bottom_candidate.top_border(), &self.bottom_border())
     }
@@ -164,6 +176,73 @@ impl OrientedTile {
             }
         }
         true
+    }
+
+    /// Highlights sea monsters with 'O' and returns the total number of sea monsters identified
+    pub fn highlight_seamonsters(&mut self) -> usize { // TODO should I return boolean instead?
+        let window_height = OrientedTile::SEA_MONSTER.len();
+        let window_width = OrientedTile::SEA_MONSTER[0].len();
+        let vertical_windows = self.pixels.len() - window_height;
+        let horizontal_windows = self.pixels.len() - window_width;
+
+        let mut sum = 0usize;
+        for i in 0..vertical_windows {
+            for j in 0..horizontal_windows {
+                if self.contains_sea_monster(i, j) {
+                    sum += 1;
+                    self.highlight_seamonster(i, j);
+                }
+            }
+        }
+        sum
+    }
+
+    fn highlight_seamonster(&mut self, vertical_offset: usize, horizontal_offset: usize) {
+        for i in 0..OrientedTile::SEA_MONSTER.len() {
+            let pattern_row = OrientedTile::SEA_MONSTER[ i ];
+            let image_row = &mut self.pixels[i + vertical_offset];
+            for j in 0..pattern_row.len() {
+                let pattern = pattern_row[j];
+                match pattern {
+                    '#' => image_row[j + horizontal_offset] = 'O',
+                    _ => {},
+                }
+            }
+        }
+    }
+
+    fn contains_sea_monster(&self, vertical_offset: usize, horizontal_offset: usize) -> bool {
+        for i in 0..OrientedTile::SEA_MONSTER.len() {
+            let pattern_row = OrientedTile::SEA_MONSTER[ i ];
+            let image_row = &self.pixels[i + vertical_offset];
+            for j in 0..pattern_row.len() {
+                let pattern = pattern_row[j];
+                // spaces can be anything
+                if pattern == '#' && image_row[ j + horizontal_offset ] != '#' {
+                    // only the '#' pixels need to match
+                    return false;
+                }
+            }
+        }
+        true
+    }
+
+    /// Determines how rough the waters are in the sea monsters' habitat
+    ///
+    /// Note the sea monsters must first be highlighted by invoking
+    /// `highlight_seamonster(&mut self, usize, usize)`.
+    ///
+    /// Returns: the number of '#' in the image that are _not_ part of a sea monster
+    pub fn roughness(&self) -> usize {
+        let mut result = 0usize;
+        for row in &self.pixels {
+            for cell in row {
+                if cell == &'#' {
+                    result += 1;
+                }
+            }
+        }
+        result
     }
 }
 
@@ -193,8 +272,8 @@ pub fn get_input() -> Vec<Tile> { // TODO can I return an Iterator?
 }
 
 pub fn get_valid_arrangements(partial_arrangement: Vec<OrientedTile>,
-                          remaining_tiles: Vec<Tile>,
-                          edge_length: usize) -> Vec<Vec<OrientedTile>> { // TODO can I return an Iterator instead?
+                              remaining_tiles: Vec<Tile>,
+                              edge_length: usize) -> Vec<Vec<OrientedTile>> { // TODO can I return an Iterator instead?
     if remaining_tiles.is_empty() {
         return vec![partial_arrangement];
     } else if partial_arrangement.is_empty() {
@@ -217,7 +296,7 @@ pub fn get_valid_arrangements(partial_arrangement: Vec<OrientedTile>,
                 }
             }
         }
-        panic!("No valid arrangements found");
+        return vec![];
     }
 
     // Find all possible suffixes given the partial, valid, arrangement
@@ -272,15 +351,43 @@ fn fits(arrangement: &[OrientedTile], candidate: &Tile, edge_length: usize) -> O
     None
 }
 
+fn combine(arrangement: &[Tile], tiles_on_edge: usize, edge_size: usize) -> Tile {
+    let mut grid: Vec<Vec<char>> = Vec::with_capacity(edge_size);
+    for (index, tile) in arrangement.iter().enumerate() {
+        let tile_row = index / tiles_on_edge;
+        let tile_column = index % tiles_on_edge;
+        let row_offset = tile.pixels.len() * tile_row;
+        let column_offset = tile.pixels.len() * tile_column;
+        for original_row in 0..tile.pixels.len() {
+            let row_id = original_row + row_offset;
+            for (original_column, pixel) in tile.pixels[original_row].iter().enumerate() {
+                let column_id = original_column + column_offset;
+                if column_id == 0 {
+                    grid.push(Vec::with_capacity(edge_size));
+                }
+                let row = &mut grid[row_id];
+                row.push(*pixel);
+            }
+        }
+    }
+    Tile {
+        id: 0,
+        pixels: grid,
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::day20::{get_input, get_valid_arrangements};
+    use crate::day20::{combine, get_input, get_valid_arrangements, Tile};
+
+// TODO extract common code to pull arrangement once
 
     #[test]
     fn part1() {
         let tiles = get_input();
         let edge_length = (tiles.len() as f32).sqrt() as usize;
         let possible_arrangements = get_valid_arrangements(vec![], tiles, edge_length);
+        assert!(possible_arrangements.len() >= 1);
         let arrangement = possible_arrangements.get(0).unwrap();
         let top_left = arrangement.get(0).unwrap();
         let top_right = arrangement.get(edge_length - 1).unwrap();
@@ -292,4 +399,38 @@ mod tests {
         println!("Part 1: {}", result);
     }
 
+    #[test]
+    fn part2() {
+        let tiles = get_input();
+        let tiles_on_edge = (tiles.len() as f32).sqrt() as usize;
+        let edge_size = tiles.get(0).unwrap().pixels.len();
+        let possible_arrangements = get_valid_arrangements(vec![], tiles, tiles_on_edge);
+        assert!(possible_arrangements.len() >= 1);
+        let arrangement = possible_arrangements.get(0).unwrap();
+
+        let cropped_tiles =
+            arrangement.iter().map(|tile| -> Tile { // TODO add Tile method to remove border
+                let mut wide_rows = tile.pixels.clone();
+                wide_rows.remove(wide_rows.len() - 1);
+                wide_rows.remove(0);
+
+                Tile {
+                    id: tile.id,
+                    pixels: wide_rows.iter().map(|wide_row| -> Vec<char> {
+                        let mut row = wide_row.clone();
+                        row.remove(row.len() - 1);
+                        row.remove(0);
+                        row
+                    }).collect(),
+                }
+            }).collect::<Vec<Tile>>();
+        let combined_tile = combine(&cropped_tiles, tiles_on_edge, edge_size);
+        for mut permutation in combined_tile.permutations() {
+            let num_sea_monsters = permutation.highlight_seamonsters();
+            if num_sea_monsters > 0 {
+                println!("Part 2: {}", permutation.roughness());
+                break;
+            }
+        }
+    }
 }
