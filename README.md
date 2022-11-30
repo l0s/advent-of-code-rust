85cc523d1684ca9373729b0e35789c4ddc937fbd
Carlos Macasaet <github@carlos-m.net>
2022-11-29 21:20:14 -0800

    Prepare for 2022

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Advent of Code 2020 (Rust)
+# Advent of Code 2022 (Rust)
 
 ## Other Editions
 
-* 2020 Advent of Code ([Java](https://github.com/l0s/advent-of-code-java))
+* 2020 Advent of Code ([Java](https://github.com/l0s/advent-of-code-java/tree/2020)|[Rust](https://github.com/l0s/advent-of-code-rust/releases/tag/y2020))
--- src/day01.rs
+++ src/day01.rs
@@ -1,66 +1,23 @@
 use crate::get_lines;
 
-pub fn get_input() -> Vec<u16> {
-    let mut items: Vec<u16> = get_lines("day-1-input.txt")
-        .map(|line| line.parse::<u16>().unwrap())
-        .filter(|candidate| *candidate <= 2020u16) // filter out anything that is obviously too big
-        .collect();
-    items.sort_unstable(); // sort to reduce the search space for the complement
-    items
+pub fn get_input() -> Vec<String> {
+    get_lines("day-1-input.txt").collect()
 }
 
 #[cfg(test)]
 mod tests {
-    use std::cmp::Ordering;
 
-    #[test]
+    // #[test]
     fn part1() {
-        let items = super::get_input();
+        let _items = super::get_input();
 
-        for i in 0..items.len() - 1 {
-            let x: u32 = items[i].into();
-            for y in items.iter().skip(i + 1) {
-                let y = *y as u32;
-                let result = x + y;
-                match result.cmp(&2020) {
-                    Ordering::Equal => {
-                        println!("Part 1: {}", (x * y));
-                        return;
-                    }
-                    Ordering::Greater => {
-                        break;
-                    }
-                    Ordering::Less => {}
-                }
-            }
-        }
+        todo!()
     }
 
-    #[test]
+    // #[test]
     fn part2() {
-        let items = super::get_input();
-        for i in 0..items.len() - 2 {
-            let x: u32 = items[i].into();
-            for j in i + 1..items.len() - 1 {
-                let y: u32 = items[j].into();
-                if x + y > 2020 {
-                    break;
-                }
-                for z in items.iter().skip(j + 1) {
-                    let z = *z as u32;
-                    let result = x + y + z;
-                    match result.cmp(&2020u32) {
-                        Ordering::Less => {}
-                        Ordering::Equal => {
-                            println!("Part 2: {}", (x * y * z));
-                            return;
-                        }
-                        Ordering::Greater => {
-                            break;
-                        }
-                    }
-                }
-            }
-        }
+        let _items = super::get_input();
+
+        todo!()
     }
 }
