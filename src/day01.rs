445cfba9d6e10de9199e76253c3c0c2711cb6a79
Carlos Macasaet <github@carlos-m.net>
2022-11-30 22:24:29 -0800

    Day 1 - performance optimisation

--- src/day01.rs
+++ src/day01.rs
@@ -1,55 +1,81 @@
 use crate::get_lines;
+use std::cmp::Ordering;
 
-pub fn get_elves() -> Vec<Elf> {
-    let mut items = vec![];
+pub fn get_elves(max: usize) -> Vec<Elf> {
+    let mut calories_carried = 0usize;
     let mut result = vec![];
     for line in get_lines("day-01.txt") {
         if line.is_empty() {
-            result.push(Elf {
-                item_calories: items,
-            });
-            items = vec![];
+            let elf = Elf { calories_carried };
+            calories_carried = 0;
+
+            let index = match result.binary_search(&elf) {
+                Ok(index) => index,
+                Err(index) => index,
+            };
+            result.insert(index, elf);
+            if result.len() > max {
+                result.remove(result.len() - 1);
+            }
         } else {
-            items.push(line.parse::<usize>().unwrap());
+            calories_carried += line.parse::<usize>().unwrap();
         }
     }
-    if !items.is_empty() {
-        result.push(Elf {
-            item_calories: items,
-        });
+    if calories_carried > 0 {
+        let elf = Elf { calories_carried };
+        let index = match result.binary_search(&elf) {
+            Ok(index) => index,
+            Err(index) => index,
+        };
+        result.insert(index, elf);
+        if result.len() > max {
+            result.remove(result.len() - 1);
+        }
     }
     result
 }
 
+#[derive(Debug)]
 pub struct Elf {
-    item_calories: Vec<usize>,
+    calories_carried: usize,
 }
 
-impl Elf {
-    pub fn calories_carried(&self) -> usize {
-        self.item_calories.iter().sum()
+impl Eq for Elf {}
+
+impl PartialEq<Self> for Elf {
+    fn eq(&self, other: &Self) -> bool {
+        self.calories_carried == other.calories_carried
+    }
+}
+
+impl PartialOrd<Self> for Elf {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        other.calories_carried.partial_cmp(&self.calories_carried)
+    }
+}
+
+impl Ord for Elf {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.calories_carried.cmp(&self.calories_carried)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::day01::{get_elves, Elf};
+    use crate::day01::get_elves;
 
     #[test]
     fn part1() {
-        let mut elves = get_elves();
-        elves.sort_unstable_by(|x, y| y.calories_carried().cmp(&x.calories_carried()));
+        let elves = get_elves(1);
 
-        println!("Part 1: {}", elves[0].calories_carried());
+        println!("Part 1: {}", elves[0].calories_carried);
     }
 
     #[test]
     fn part2() {
-        let mut elves = get_elves();
-        elves.sort_unstable_by(|x, y| y.calories_carried().cmp(&x.calories_carried()));
+        let elves = get_elves(3);
+        let result: usize = elves.iter().map(|elf| elf.calories_carried).sum();
 
-        let (elves, _) = elves.split_at(3);
-        let result: usize = elves.iter().map(Elf::calories_carried).sum();
         println!("Part 2: {}", result);
     }
 }
